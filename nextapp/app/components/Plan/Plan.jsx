9f74c9079603258f22e471ec8d1beb95cd869411
Igor Parfenov <lloopy123123543@gmail.com>
2023-09-25 18:07:22 +0500

    update

--- nextapp/app/components/Plan/Plan.jsx
+++ nextapp/app/components/Plan/Plan.jsx
@@ -8,8 +8,8 @@ export default function Plan() {
                 <div id='start' style={{zIndex:10}} className={stylePlan.block}>
 
 <div>
-<h1>Fix issues fast and get expert insights for your business</h1>
-<p>All Stripe customers receive 24x7 phone, email, and chat help plus access to support center resources. Our support plans give you additional guidance with access to designated technical and support teams at Stripe.</p>
+<h1>Данные о продажах и марженальности на маркетплейсах</h1>
+<p>Получайте ценные данные о продажах и марженальности товаров на ведущих маркетплейсах. Принимайте осознанные решения, анализируя актуальные отчеты и оптимизируя свою стратегию. Добейтесь большего успеха на маркетплейсе с нашим сервисом.</p>
 <a href="#contacts"><button>Свяжитесь с нами</button></a>
 </div>
 </div>
